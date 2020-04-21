3c85d286497b55e378d3035502e4e989fec20437
Maria Litovchenko <maria.litovchenko@gmail.com>
2020-04-21 17:08:20 +0200

    Trial sniny run

--- plotMapStats.R
+++ plotMapStats.R
@@ -1,6 +1,4 @@
-#==============================================================================
-#
-# FILE: R0_plot_mapping_stats.R
+# FILE: R0_plot_mapping_stats.R -----------------------------------------------
 #
 # USAGE: 
 #
@@ -15,16 +13,16 @@
 #  VERSION:  1
 #  CREATED:  06.09.2017
 #  REVISION: 06.09.2017
-#==============================================================================
 
 # !!!! IMPORTANT NOTICE !!!!
 # I MULTIPLY EVERYWHERE NUMBER OF READS BY 2, BECAUSE IT'S FROM STAR, AND IT
 # GIVES NUMBER OF PAIRS OF READS
 
-#------------------------------------------------------------------------------
-# LIBRARIES, FUNCTIONS, THEMES FOR PLOTTING
-#------------------------------------------------------------------------------
+# LIBRARIES, FUNCTIONS, THEMES FOR PLOTTING ------------------------------------------------------------------------------
+library(data.table)
 library(ggplot2)
+library(shiny)
+
 mashaGgplot2Theme <- list(
   theme_classic(base_size = 18) +
     theme(axis.line.x = element_line(colour = 'black', size = 0.5,
@@ -39,31 +37,72 @@ shadesOfGreen <- "#0C8954"
 shadesOfRed <- c("#FFD9D9", '#FFBABA', '#C48484', '#991D1D', '#730202', 
                  'black')
 
+mappingStatsPath <- 'mapStatsTab.csv'
 
-#------------------------------------------------------------------------------
-# INPUTS
-#------------------------------------------------------------------------------
-setwd('~/Desktop/Antonio_BIG/')
-mappingStatsPath <- '~/Documents/Antonio_BIG/align_to_dm6_stats.txt'
-
+# INPUTS ----------------------------------------------------------------------
+statNames <- c('RunID', 'LibraryID', 'SampleID', 'Specie', 'Genome', 
+               'SubSample', 'total reads', 'uniquely mapped', 
+               'mapped to mult. loci', 'mapped to too many loci',
+               'NOT mapped - mismatches', 'NOT mapped - too short',
+               'NOT mapped - other')
 # read-in and assign condition
-stats <- read.table(mappingStatsPath, header = T, stringsAsFactors = F)
-stats$Condition <- sapply(stats$Name,
-                          function(x) grep('P[eE]|UNC', 
-                                           unlist(strsplit(x, '_')), 
-                                           value = T))
-stats$Condition[lapply(stats$Condition, length) == 0] <- NA
-stats$Mark <- sapply(stats$Name,
-                     function(x) unlist(strsplit(x, '_'))[1])
-stats <- as.data.frame(apply(stats, 2, unlist))
+stats <- fread(mappingStatsPath, header = F, stringsAsFactors = F)
+setnames(stats, colnames(stats), statNames)
 
-stats <- stats[stats$Mark %in% c('ATAC', 'Pol2', '1'), ]
-stats$Name <- gsub('_dm6', '', stats$Name)
-stats$Name <- gsub('_2017', '', stats$Name)
+# for the subsample names, remove elements of the path from it, leaving just
+# samples
+stats[, SubSample := gsub('.*/', '', SubSample)]
+stats[, SubSample := gsub('[.].*', '', SubSample)]
+# convert mapping stats which are reported as percentage to numeric
+stats[, `NOT mapped - mismatches` := gsub('%', '', `NOT mapped - mismatches`)]
+stats[, `NOT mapped - mismatches` := as.double(`NOT mapped - mismatches`)]
+stats[, `NOT mapped - mismatches` := 0.01 * `NOT mapped - mismatches` * 
+                                     `total reads`]
+stats[, `NOT mapped - mismatches` := as.integer(`NOT mapped - mismatches`)]
+stats[, `NOT mapped - too short` := gsub('%', '', `NOT mapped - too short`)]
+stats[, `NOT mapped - too short` := as.double(`NOT mapped - too short`)]
+stats[, `NOT mapped - too short` := 0.01 * `NOT mapped - too short` * 
+                                    `total reads`]
+stats[, `NOT mapped - too short` := as.integer(`NOT mapped - too short`)]
+stats[, `NOT mapped - other` := gsub('%', '', `NOT mapped - other`)]
+stats[, `NOT mapped - other` := as.double(`NOT mapped - other`)]
+stats[, `NOT mapped - other` := 0.01 * `NOT mapped - other` * 
+                                `total reads`]
+stats[, `NOT mapped - other` := as.integer(`NOT mapped - other`)]
 
-stats[, 2:5] <- 2 * apply(stats[, 2:5], 2, as.numeric)
-stats[, 6:8] <- 0.01 * apply(stats[, 6:8], 2, as.numeric) *
-                       as.numeric(stats[, 2])
+statsMelt <- melt(stats[, -7], 
+                  id.vars = c('RunID', 'LibraryID', 'SampleID', 'Specie',
+                                     'Genome',  'SubSample'))
+
+p <- ggplot(statsMelt, aes(x = SubSample, y = value/1000000, 
+                        fill = variable)) + 
+  geom_bar(stat = "identity") + 
+  ggtitle('Number of uniquely mapped/unmapped reads') +
+  xlab("Sample") + ylab("Number of reads, mlns") + mashaGgplot2Theme +
+  scale_fill_manual("legend", 
+                    values = c("NOT mapped" = shadesOfRed[1], 
+                               "uniquely mapped" = shadesOfGreen[1], 
+                               "mapped to mult. loci" = shadesOfRed[2],
+                               'mapped to too many loci' = shadesOfRed[6],
+                               "NOT mapped - mismatches" = shadesOfRed[3],
+                               'NOT mapped - too short' = shadesOfRed[4],
+                               'NOT mapped - other' = shadesOfRed[5])) + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+# Define UI for app that draws a histogram ----
+ui <- fluidPage(titlePanel("Hello Shiny!"), # title
+                sidebarLayout(sidebarPanel(sliderInput(inputId = "bins", 
+                                           label = "Number of bins:",
+                                           min = 1,
+                                           max = 50,
+                                           value = 30)),
+                              mainPanel(# Main panel for displaying outputs
+                                plotOutput(outputId = "distPlot"))))
+server <- function(input, output) {
+  output$distPlot <- renderPlot({p})
+}
+shinyApp(ui, server)
 
 #------------------------------------------------------------------------------
 # MAPPED/UNMAPPED READS
