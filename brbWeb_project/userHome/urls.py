006d2609563726fed992fc9b6ee0f068c0c5a32a
Maria Litovchenko <maria.litovchenko@gmail.com>
2020-05-07 14:40:05 +0200

    User registration

--- brbWeb_project/userHome/urls.py
+++ brbWeb_project/userHome/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='user-home'),
+    path('userHome/', views.home, name='user-home'),
     path('tutorial/', views.tutorial, name='user-tutorial')
 ]
