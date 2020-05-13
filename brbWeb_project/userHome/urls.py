3c85e0f9088698de13a91e7b636c4d69ac483d7c
Maria Litovchenko <maria.litovchenko@gmail.com>
2020-05-13 18:48:34 +0200

    Fully working updating of projects from the website

--- brbWeb_project/userHome/urls.py
+++ brbWeb_project/userHome/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .views import (ProjectListView, 
 	ProjectDetailView, 
-	ProjectCreateView)
+	ProjectCreateView,
+	ProjectUpdateView)
 from . import views
 
 urlpatterns = [
     path('userHome/', ProjectListView.as_view(), name = 'user-home'),
     path('project/<int:pk>/', ProjectDetailView.as_view(), name = 'project-detail'),
+    path('project/<int:pk>/update/', ProjectUpdateView.as_view(), name = 'project-update'),
     path('project/new/', ProjectCreateView.as_view(), name = 'project-create'),
     path('tutorial/', views.tutorial, name = 'user-tutorial')
 ]
--- brbWeb_project/userHome/views.py
+++ brbWeb_project/userHome/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (ListView, 
 	DetailView, 
-	CreateView)
+	CreateView,
+	UpdateView)
 from .models import Project
 
 def home(request):
@@ -28,6 +29,20 @@ class ProjectCreateView(LoginRequiredMixin, CreateView):
 	def form_valid(self, form):
 		form.instance.author = self.request.user
 		return super().form_valid(form)
+
+class ProjectUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	model = Project
+	fields = ['name', 'description']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		project = self.get_object()
+		if self.request.user == project.author :
+			return True
+		return False
 
 def tutorial(request):
     return render(request, 'userHome/tutorial.html', {'title': 'Test title'})
