592aeefa122c8a02f457b4b370fa05f914700c7e
Maria Litovchenko <maria.litovchenko@gmail.com>
2020-05-14 14:22:07 +0200

    Fixed error in registration form

--- brbWeb_project/users/views.py
+++ brbWeb_project/users/views.py
@@ -4,17 +4,17 @@ from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 def register(request):
-	if request.method == 'POST':
-		form = UserRegisterForm(request.POST)
-		if form.is_valid():
-			form.save()
-			username = form.cleaned_data.get('username')
-			messages.success(request, f'Your account has been created. You are now able to log in.')
-			return redirect('Log in')
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('Log in')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
 
-	else:
-		form = UserRegisterForm()
-	return render(request, 'users/register.html', {'form': form})
 
 @login_required 
 def profile(request):
