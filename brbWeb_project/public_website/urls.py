4e59bb11230eb253e68454c05f9253a327ae3b3c
Maria Litovchenko <maria.litovchenko@gmail.com>
2020-05-14 13:42:40 +0200

    Renamed public_home to index

--- brbWeb_project/public_website/urls.py
+++ brbWeb_project/public_website/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.public_home, name = 'public_home')
+    path('', views.index, name = 'index')
 ]
--- brbWeb_project/public_website/views.py
+++ brbWeb_project/public_website/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def public_home(request):
-    return render(request, 'public_website/public_home.html', {'title': 'Alithea Genomics'})
+def index(request):
+    return render(request, 'public_website/index.html', {'title': 'Alithea Genomics'})
