2a6a020841bb98ac672b723b6d90ea5bfc829811
Maria Litovchenko <maria.litovchenko@gmail.com>
2020-05-15 13:23:49 +0200

    Started table view

--- brbWeb_project/brbWeb_project/settings.py
+++ brbWeb_project/brbWeb_project/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'userHome.apps.UserhomeConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
+    'django_tables2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
